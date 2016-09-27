156828d17e1cebadd63aaf0e2d878cacaca4a835
Richard lavallee <rllavallee@hotmail.com>
2016-09-27 13:19:52 -0700

    Added GET todos

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ var express = require('express');
 var app = express();
 const PORT = 3000;
 
-//app.get('/', function (req, res) {
-//	res.send('heelo express!');
-//});
+var todos = [{
+	id: 1,
+	description: 'Meet mom for lucnh',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed: false
+}, {
+	id: 3,
+	description: 'Feed the cat',
+	completed: true
+}];
+
+// GET /todos
+app.get('/todos', function (req, res) {
+   res.json(todos);
+});
+
+// GET /todos/:id
 
 app.use(express.static(__dirname + '/public'));
 
