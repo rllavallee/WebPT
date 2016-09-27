44311bfb599047e22ea93346ae74497fa6079aac
Richard lavallee <rllavallee@hotmail.com>
2016-09-27 13:44:05 -0700

    Added todo api

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 var todos = [{
 	id: 1,
-	description: 'Meet mom for lucnh',
+	description: 'Meet mom for lunch',
 	completed: false
 }, {
 	id: 2,
@@ -22,6 +22,24 @@ app.get('/todos', function (req, res) {
 });
 
 // GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+	
+	var todoId = parseInt(req.params.id);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+
+		if (todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	})
+
+	if (matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+});
 
 app.use(express.static(__dirname + '/public'));
 
